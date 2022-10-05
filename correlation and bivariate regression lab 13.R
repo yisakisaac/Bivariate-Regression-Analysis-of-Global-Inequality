a96f439dc2c5bcc67de018758c898bc20174dd36
yisakisaac <115116705+yisakisaac@users.noreply.github.com>
2022-10-05 14:25:19 -0700

    Update correlation and bivariate regression lab 13.R

--- correlation and bivariate regression lab 13.R
+++ correlation and bivariate regression lab 13.R
@@ -1,6 +1,6 @@
 library(poliscidata)
 
-#Question 1 2012 voter turnout correlations
+#2012 voter turnout correlations
 names(states)
   #positively correlated variable
   cor(states$vep12_turnout,states$ba_or_more)
@@ -9,10 +9,10 @@ names(states)
   #uncorrelated variable
   cor(states$vep12_turnout,states$unemploy)
   
-#Question 4 bivariate regression analysis of global inequality 
+#bivariate regression analysis of global inequality 
 summary(lm(world$gini10~world$gdp_10_thou))
 
-#Question 6 scatterplot and regression line
+#scatterplot and regression line
 plot(world$gdp_10_thou,world$gini10)
 plot(world$gdp_10_thou,world$gini10,xlab="GDP per capita in $10K",ylab="Income GINI Coefficient",main="Bivariate Regression Analysis of Global Inequality")
 abline(lm(world$gini10~world$gdp_10_thou))
